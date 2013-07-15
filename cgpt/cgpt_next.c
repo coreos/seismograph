b2271093d6442392b7fd18858d46061c4558a34e
Brandon Philips <brandon@ifup.co>
2013-07-15 11:18:20 -0700

    feat(cgpt/cpgt_next): add a new cgpt command
    cgpt next will return the partition uuid of the next partition that
    should be booted. This can be used in an initrd to figure out what root
    partition to use.
    
    It handles decrementing tries so this is a one shot command.

---
+++ cgpt/cgpt_next.c
@@ -0,0 +1,128 @@
+// Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <string.h>
+
+#include "cgpt.h"
+#include "cgptlib_internal.h"
+#include "vboot_host.h"
+
+#define BUFSIZE 1024
+
+char next_file_name[BUFSIZE];
+int next_priority, next_index;
+
+static int do_search(CgptNextParams *params, char *fileName) {
+  struct drive drive;
+  uint32_t max_part;
+  int gpt_retval;
+  int priority, tries, successful;
+  int i;
+
+  // TODO: don't hard code this
+  if (CGPT_OK != DriveOpen(fileName, &drive, O_RDONLY))
+    return CGPT_FAILED;
+
+  if (GPT_SUCCESS != (gpt_retval = GptSanityCheck(&drive.gpt))) {
+    Error("GptSanityCheck() returned %d: %s\n",
+          gpt_retval, GptError(gpt_retval));
+    return CGPT_FAILED;
+  }
+
+  max_part = GetNumberOfEntries(&drive);
+
+  for (i = 0; i < max_part; i++) {
+    if (!IsRoot(&drive, PRIMARY, i))
+      continue;
+
+    priority = GetPriority(&drive, PRIMARY, i);
+    tries = GetTries(&drive, PRIMARY, i);
+    successful = GetSuccessful(&drive, PRIMARY, i);
+
+    if (next_index == -1 || ((priority > next_priority) && (successful || tries))) {
+      strncpy(next_file_name, fileName, BUFSIZE);
+      next_priority = priority;
+      next_index = i;
+    }
+  }
+
+  return DriveClose(&drive, 0);
+}
+
+#define PROC_PARTITIONS "/proc/partitions"
+
+// This scans all the physical devices it can find, looking for a match. It
+// returns true if any matches were found, false otherwise.
+static int scan_real_devs(CgptNextParams *params) {
+  int found = 0;
+  char line[BUFSIZE];
+  char partname[128];                   // max size for /proc/partition lines?
+  FILE *fp;
+  char *pathname;
+
+  fp = fopen(PROC_PARTITIONS, "r");
+  if (!fp) {
+    perror("can't read " PROC_PARTITIONS);
+    return found;
+  }
+
+  while (fgets(line, sizeof(line), fp)) {
+    int ma, mi;
+    long long unsigned int sz;
+
+    if (sscanf(line, " %d %d %llu %127[^\n ]", &ma, &mi, &sz, partname) != 4)
+      continue;
+
+    if ((pathname = IsWholeDev(partname))) {
+      do_search(params, pathname);
+    }
+  }
+
+  fclose(fp);
+  return found;
+}
+
+int CgptNext(CgptNextParams *params) {
+  struct drive drive;
+  GptEntry *entry;
+  char tmp[64];
+  int tries;
+  next_index = -1;
+  int gpt_retval;
+
+  if (params == NULL)
+    return CGPT_FAILED;
+
+  scan_real_devs(params);
+
+  if (strlen(next_file_name) < 0) {
+    return CGPT_FAILED;
+  }
+
+  if (DriveOpen(next_file_name, &drive, O_RDWR) == CGPT_OK) {
+    if (GPT_SUCCESS != (gpt_retval = GptSanityCheck(&drive.gpt))) {
+      Error("GptSanityCheck() returned %d: %s\n",
+            gpt_retval, GptError(gpt_retval));
+      return CGPT_FAILED;
+    }
+
+    // Decrement tries if we selected on that criteria
+    tries = GetTries(&drive, PRIMARY, next_index);
+    if (tries > 0) {
+      tries--;
+    }
+    SetTries(&drive, PRIMARY, next_index, tries);
+
+    // Print out the next disk to go!
+    entry = GetEntry(&drive.gpt, ANY_VALID, next_index);
+    GuidToStr(&entry->unique, tmp, sizeof(tmp));
+    printf("%s\n", tmp);
+
+    // Write it all out
+    UpdateAllEntries(&drive);
+    return DriveClose(&drive, 1);
+  }
+
+  return 1;
+}
---
+++ cgpt/cmd_next.c
@@ -0,0 +1,63 @@
+// Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <uuid/uuid.h>
+
+#include "cgpt.h"
+#include "vboot_host.h"
+
+static void Usage(void)
+{
+  printf("\nUsage: %s next type\n\n"
+         "Look at all of the disks and find the disk UUID we should attempt.\n\n"
+         "\n", progname);
+}
+
+int cmd_next(int argc, char *argv[]) {
+  CgptNextParams params;
+  memset(&params, 0, sizeof(params));
+
+  int c;
+  int errorcnt = 0;
+
+  opterr = 0;                     // quiet, you
+  while ((c=getopt(argc, argv, ":hi:b:p")) != -1)
+  {
+    switch (c)
+    {
+    case 'h':
+      Usage();
+      return CGPT_OK;
+    case '?':
+      Error("unrecognized option: -%c\n", optopt);
+      errorcnt++;
+      break;
+    case ':':
+      Error("missing argument to -%c\n", optopt);
+      errorcnt++;
+      break;
+    default:
+      errorcnt++;
+      break;
+    }
+  }
+  if (errorcnt)
+  {
+    Usage();
+    return CGPT_FAILED;
+  }
+
+  if (optind >= argc) {
+    Error("missing drive argument\n");
+    return CGPT_FAILED;
+  }
+
+  params.drive_type = argv[optind];
+
+  return CgptNext(&params);
+}
